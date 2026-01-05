d81469023d0bac83c9641e1129215fd6fb8f8ddf
ccoin27 <coin8020@gmail.com>
2026-01-05 22:40:06 +0100

    w

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
-# Multi-Server Repo for Pterodactyl
-
-By https://realmnodes.space
-
-This repository contains configs and scripts for running nginx+php, node.js, and python servers.
-
-Set environment variables in Pterodactyl egg to control which server to start:
-
-- USE_NODE=1 to start Node.js
-- USE_PYTHON=1 to start Python Flask
-- Else default runs nginx + php-fpm
+# Установка сайта на хостинге satx.cloud
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env?.PORT;
+
+if (!port) {
+  return console.log(`Порт не найден, замените его вручную в server.js\nНайти порт можно в блоке "адрес"`);
+}
+
+const webrootPath = path.join(__dirname, 'webroot');
+
+app.use(express.static(webrootPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(webrootPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Сервер успешно запустился на порту: ${port}\nЧто-бы зайти на ваш сайт привяжите домен во вкладке "Домены"`);
+});
