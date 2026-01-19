764d6a3d386bcca70de6662d1c4f069fd21a77e3
ccoin27 <coin8020@gmail.com>
2026-01-19 16:44:34 +0100

    w

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
-const port = PORT_PLACEHOLDER_INDIVIDUAL;
+const port = process.env.SERVER_PORT;
 
 if (!port) {
-  return console.log(`Порт не найден, замените его вручную в server.js\nНайти порт можно в блоке "адрес"`);
+  console.log(
+    'Порт не найден.\n' +
+    'Убедитесь, что переменная окружения SERVER_PORT задана.\n' +
+    'Найти порт можно в блоке "адрес".'
+  );
+  process.exit(1);
 }
 
-const webrootPath = path.join(__dirname, 'webroot');
+const sites = {
+  'domain1': 'site1',
+  'domain2': 'site2',
+};
 
-app.use(express.static(webrootPath));
+app.use((req, res, next) => {
+  const host = req.hostname;
+  const siteFolder = sites[host] || 'default';
+
+  req.sitePath = path.join(__dirname, 'webroot', siteFolder);
+  next();
+});
+
+app.use((req, res, next) => {
+  express.static(req.sitePath)(req, res, next);
+});
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(webrootPath, 'index.html'));
+  res.sendFile(path.join(req.sitePath, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Сервер успешно запустился на порту: ${port}\nЧто-бы зайти на ваш сайт привяжите домен во вкладке "Домены"`);
+  console.log(
+    `Сервер успешно запустился на порту: ${port}\n`
+  );
 });
